46ccf163829a791223f93e6adf1ed0333cfa5a36
Jerson <jersonos@outlook.es>
2025-01-28 11:28:00 -0700

    Se comenzo a experimentar con Tkinter, se agregaron 3 frames boceto

--- tamapp.py
+++ tamapp.py
@@ -1,1 +1,21 @@
-print("Esto es una prueba para hacer un commit")
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.title("Tamapp")
+
+frame1 = ttk.Frame(root,width=300, height=200, padding="20 20 20 20")
+frame1.grid(column=0, row=0, sticky= (N, W, E, S))
+
+frame2 = ttk.Frame(root,width=300, height=200, padding="20 20 20 20")
+frame2.grid(column=0, row=1, sticky= (N, W, E, S))
+
+frame3 = ttk.Frame(root,width=300, height=200, padding="20 20 20 20")
+frame3.grid(column=0, row=2, sticky= (N, W, E, S))
+
+
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+root.mainloop()
+
