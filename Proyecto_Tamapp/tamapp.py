674224590d284015f10341d16544b5db93e25248
Jerson <jersonos@outlook.es>
2025-02-16 20:19:40 -0700

    Se comenzo a agregarle funcion a los botones

--- Proyecto_Tamapp/tamapp.py
+++ Proyecto_Tamapp/tamapp.py
@@ -1,4 +1,8 @@
 from tkinter import *
+import os
+
+print(os.listdir())  # Muestra los archivos en el directorio actual
+
 #Creacion de la ventana raiz y su logo
 ventana_Principal = Tk()
 ventana_Principal.geometry("289x537")
@@ -10,12 +14,21 @@ ventana_Principal.title("Tamapp")
 titulo_Principal = Label(ventana_Principal,text="Tamapp")
 titulo_Principal.grid(row=0,column=1)
 
+#Funciones
+def ventana_Registro():
+    ventana = Toplevel(ventana_Principal)
+    mensaje_Ventana_Registro = Label(ventana,text="Ventana de registro de productos")
+    mensaje_Ventana_Registro.pack()
+    
 
+#Alineacion de los botones
 boton_Tamales = Button(ventana_Principal,text="Tamales").grid(row=1,column=0)
-boton_Pan = Button(ventana_Principal,text="Pan").grid(row=2,column=0)
+boton_Pan = Button(ventana_Principal,text="Pan", command=ventana_Registro).grid(row=2,column=0)
 boton_Donas = Button(ventana_Principal,text="Donas").grid(row=3,column=0)
 boton_ChampurradoGde = Button(ventana_Principal,text="Champurrado Gde").grid(row=4,column=0)
 boton_ChampurradoChi = Button(ventana_Principal,text="Champurrado Chi").grid(row=5,column=0)
+
+
 
 #Loop grafico
 ventana_Principal.mainloop()
